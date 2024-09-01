05d154cf45cff59d345bb94ee21ab513c9dc5795
kmth-hash <bpk.san4ya@gmail.com>
2024-09-01 18:13:15 +0530

    Add Navbar UI

---
+++ testapp1/src/Navbar.js
@@ -0,0 +1,13 @@
+
+
+function Navbar(){
+    return(
+        <nav className="navbar bg-dark bg-body-tertiary" data-bs-theme="dark">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Test App </a>
+        </div>
+        </nav>
+    )
+}
+
+export default Navbar;
